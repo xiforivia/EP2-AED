8be70b5915e47ceb891707dc0168deb19d22df74
Juliana <you@example.com>
2021-11-23 02:19:35 -0300

    Inserção completa

--- filapreferencial.c
+++ filapreferencial.c
@@ -70,13 +70,61 @@ bool consultarPreferencial(PFILA f, int id){
 
 
 
-bool inserirPessoaNaFila(PFILA f, int id, bool ehPreferencial){
+bool inserirPessoaNaFila(PFILA f, int id, bool ehPreferencial)
+{
+	if(id < 0 || buscarID(f, id) != NULL)
+		return false;
+	
+	PONT novo;
+  	novo = (PONT) malloc(sizeof(ELEMENTO));
+	
+	novo->id = id;
+	novo->ehPreferencial = ehPreferencial;
 
-	/* COMPLETE */
+	if(f->cabeca->prox == f->cabeca && f->cabeca->ant == f->cabeca) //se a fila estiver vazia
+	{
+		f->cabeca->prox = novo;
+		f->cabeca->ant = novo;
+		novo->prox = f->cabeca; //ou novo->prox = f->cabeca->prox
+		novo->ant = f->cabeca;
+		if(ehPreferencial == false)
+			f->inicioNaoPref = novo;
+	}
+	else //se a fila não está vazia
+	{
+		if(ehPreferencial == true)
+		{
+			PONT atual = f->cabeca->ant;
+			while (atual != f->cabeca)
+			{
+				// if(atual->id == novo->id)
+				// 	break;
+				if(atual->ehPreferencial == true) //pega o ultimo preferencial
+				{
+					novo->prox = atual->prox;
+					novo->ant = atual;
+					atual->prox = novo;
+					f->inicioNaoPref->ant = novo;
+					return true;
+				}
+				atual = atual->ant;
+			}
+			//se não achou nenhum preferencial, então será o primeiro preferencial
+			novo->prox = f->inicioNaoPref;
+			novo->ant = f->cabeca;
+			f->cabeca->prox = novo;
+			f->inicioNaoPref->ant = novo;
+		}
+		else //não é preferencial = insere no fim
+		{
+			novo->prox = f->cabeca;
+			novo->ant = f->cabeca->ant;
+			f->cabeca->ant = novo;
+			novo->ant->prox = novo;
+		}
+	}
 
-
-
-	return false;
+	return true;
 }
 
 bool atenderPrimeiraDaFila(PFILA f, int* id){
@@ -95,4 +143,22 @@ bool desistirDaFila(PFILA f, int id){
 
 	return false;
 }
+/*
+int main() {
+	PFILA f = criarFila();
+	int id;
+	bool ehPreferencial;
+	bool res;
 
+	res = inserirPessoaNaFila(f, 1, false);
+	if(res) printf("Insercao retornou true (0). [OK]\n");
+	else printf("Insercao retornou false (0).\n");
+	res = inserirPessoaNaFila(f, 2, false);
+	if(res) printf("Insercao retornou true (0).\n");
+	else printf("Insercao retornou false (0). [OK]\n");
+	res = inserirPessoaNaFila(f, 0, true);
+	exibirLog(f);
+
+	return 0;
+}
+*/
